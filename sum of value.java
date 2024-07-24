5bc587e05609c6018f1d4e7ee64fdb85f3562d45
Harishwaran2002 <146076518+Harishwaran2002@users.noreply.github.com>
2024-07-24 09:16:31 +0530

    sum of value.java

---
+++ sum of value.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int num =58;
+        int firstDigit =num/10;
+        int secondDigit=num%10;
+        int sum=firstDigit+secondDigit;
+        System.out.println("Sum of digits:"+sum);
+    }
+}
